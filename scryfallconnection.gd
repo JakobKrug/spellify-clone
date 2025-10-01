17a02bf7bb71b084ddb4172f0944e7ba30c87d26
Jakob Krug <jakobkrug04@gmail.com>
2025-10-01 20:39:53 +0200

    Text on Card is now obfuscated and only Vintage legal cards are fetched

--- scryfallconnection.gd
+++ scryfallconnection.gd
@@ -1,14 +1,18 @@
 extends Node 
 class_name ScryfallHttpRequest
 var http_request
-var cardtext
-var cardname
+var cardname : Label
+var cardtype : Label
+var cardtext : Label
+var cardstats : Label
 var button
 var manaCost
 
 func _ready() -> void:
-	cardtext = $"../VBoxContainer/Card/Cardtext"
 	cardname = $"../VBoxContainer/Card/Cardname"
+	cardtype = $"../VBoxContainer/Card/Cardtype"
+	cardtext = $"../VBoxContainer/Card/Cardtext"
+	cardstats = $"../VBoxContainer/Card/CardPowerToughness"
 	button = $"../VBoxContainer/Button"
 	manaCost = $"../VBoxContainer/Card/HBoxContainer"
 	button.pressed.connect(_get_card)
@@ -17,10 +21,25 @@ func _get_card():
 	http_request = HTTPRequest.new()
 	add_child(http_request)
 	http_request.request_completed.connect(_get_callback)
-	http_request.request("https://api.scryfall.com/cards/random",["Accept: */*", "User-Agent: MTGCardGuessing/1"])
+	http_request.request(r"https://api.scryfall.com/cards/random?q=f%3Av",["Accept: */*", "User-Agent: MTGCardGuessing/1"])
 
-func _get_callback(result, response_code, headers, body):
-	var json = JSON.parse_string(body.get_string_from_utf8())
-	cardname.text = (json["name"])
-	#label.text = body.get_string_from_utf8()
-	print(body.get_string_from_utf8())
+func _get_callback(_result, _response_code, _headers, body):
+	var json : Dictionary = JSON.parse_string(body.get_string_from_utf8())
+	cardname.text = _strip_characters(json["name"])
+	cardtype.text = _strip_characters(json["type_line"])
+	cardtext.text = _strip_characters(json["oracle_text"])
+	if "Creature" in json["type_line"]:
+		cardstats.show()
+		cardstats.text = _strip_characters(json["power"]) + "/" + _strip_characters(json["toughness"])
+	else:
+		cardstats.hide()
+
+func _strip_characters(string : String) -> String:
+	print(string)
+	var regex_braces := RegEx.new()
+	regex_braces.compile(r"\{.*?\}")
+	var tap_symbol_string = regex_braces.sub(string, "○", -1)
+	var regex := RegEx.new()
+	regex.compile(r"[^\n /:?!—○,.()]")
+	var stripped_text : String = regex.sub(tap_symbol_string, "_ ", -1)
+	return stripped_text.replace("  ", "   ")
