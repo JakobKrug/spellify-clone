f5766c3de03fcb635fcb9834931cfbf133710073
Jakob Krug <jakobkrug04@gmail.com>
2025-10-02 15:16:50 +0200

    chore: restructured script files

---
+++ game_logic.gd
@@ -0,0 +1,66 @@
+extends Node
+class_name GameLogic
+
+var scryfall_fetch: Scryfall
+var manaTextureGenerator: ManaTextureGenerator
+var card : TextureRect
+var cardname : Label
+var cardtype : Label
+var cardtext : Label
+var cardstats : Label
+var button
+var manaCost
+var card_background_path = "res://cardBackgrounds/"
+
+func _ready():
+    scryfall_fetch = $ScryfallFetch
+    manaTextureGenerator = $ManaTextureGenerator
+    card = $"../VBoxContainer/Card"
+    cardname = $"../VBoxContainer/Card/Cardname"
+    cardtype = $"../VBoxContainer/Card/Cardtype"
+    cardtext = $"../VBoxContainer/Card/Cardtext"
+    cardstats = $"../VBoxContainer/Card/CardPowerToughness"
+    button = $"../VBoxContainer/Button"
+    manaCost = $"../VBoxContainer/Card/HBoxContainer"
+    manaTextureGenerator.manaCost = manaCost
+    scryfall_fetch.card_fetched.connect(_on_card_fetched)
+    button.pressed.connect(_on_button_pressed)
+
+func _on_button_pressed():
+    scryfall_fetch.fetch_random_card()
+
+func _on_card_fetched(json: Dictionary):
+    if json.has("mana_cost"):
+        manaTextureGenerator._init_card_mana(json["mana_cost"])
+    if json.has("name"):
+        cardname.text = _strip_characters(json["name"])
+    if json.has("type_line"):
+        var type : String = json["type_line"]
+        var colors : Array = json["colors"]
+        cardtype.text = _strip_characters(type)
+        var suffix := "-creature.png" if "Creature" in type else "-noncreature.png"
+        var color_map = {
+            "W" : "white",
+            "U" : "blue",
+            "B" : "black",
+            "R" : "red",
+            "G" : "green"
+        }
+        var color = color_map.get(colors[0], "colorless") if colors.size() > 0 else "colorless"
+        card.texture = load(card_background_path + color + suffix)
+    if json.has("oracle_text"):
+        cardtext.text = _strip_characters(json["oracle_text"])
+    if "Creature" in json["type_line"]:
+        cardstats.show()
+        cardstats.text = _strip_characters(json["power"]) + "/" + _strip_characters(json["toughness"])
+    else:
+        cardstats.hide()
+
+func _strip_characters(string : String) -> String:
+    var regex_braces := RegEx.new()
+    regex_braces.compile(r"\{.*?\}")
+    var tap_symbol_string = regex_braces.sub(string, "○", -1)
+    var regex := RegEx.new()
+    regex.compile(r"[^\n /:?!—○,.()]")
+    var stripped_text : String = regex.sub(tap_symbol_string, "_ ", -1)
+    return stripped_text.replace("  ", "   ")
