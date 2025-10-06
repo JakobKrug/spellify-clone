94c860e683963ca3a0ce20f37c1f5304abf14952
Jakob Krug <jakobkrug04@gmail.com>
2025-10-06 11:04:20 +0200

    dummes scheiß settings wird da gedingst alter ich kriegs kotzen

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -3,22 +3,21 @@ extends VBoxContainer
 @export var scryfall : Scryfall
 
 func _ready() -> void:
-    _make_filter_button("Land")
-    _make_filter_button("Artifact")
-    _make_filter_button("Creature")
-    _make_filter_button("Enchantment")
-    _make_filter_button("Instant")
-    _make_filter_button("Sorcery")
+	_make_filter_button("Land")
+	_make_filter_button("Artifact")
+	_make_filter_button("Creature")
+	_make_filter_button("Enchantment")
+	_make_filter_button("Instant")
+	_make_filter_button("Sorcery")
 
 func _make_filter_button(string : String):
-    var btn := CheckButton.new()
-    btn.text = string
-    add_child(btn)
+	var btn := CheckButton.new()
+	btn.text = string
+	add_child(btn)
 
-    var key = name.to_lower()
+	var key = name.to_lower()
 
-    btn.toggled.connect(func(pressed):
-        scryfall.filters[key] = pressed
-        scryfall._build_query()
-        print("Filter changed:", scryfall.query)
-    )
+	btn.toggled.connect(func pressed(button : bool):
+		scryfall.filters[key] = button
+		scryfall._build_query()
+	)
